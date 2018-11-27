b936d76bc48351ffcdbd9767a360c1efb47bdfc5
Артем Жеков <jekins@MacBook-Pro-Jekins.local>
2018-11-27 18:18:25 +0300

    Order complete page

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-/*! Englishfishing 17-11-2018 | Front-end: Jekins */
+/*! Englishfishing 27-11-2018 | Front-end: Jekins */
 /* close-outside ***********************/
 function closeOutside(el, activeClass) {
   $('html').on('click', function(e) {
